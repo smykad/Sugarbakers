2ca030f61937b269db6f8752da8272ae90e1e945
Doug <70054506+smykad@users.noreply.github.com>
2022-11-26 05:49:49 -0500

    Add project files.

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sugarbakers.Models;
+
+namespace Sugarbakers.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly SugarbakersDBContext _context;
+
+        public PaymentController(SugarbakersDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Payment
+        public async Task<IActionResult> Index()
+        {
+            var sugarbakersDBContext = _context.Payments.Include(p => p.Customer);
+            return View(await sugarbakersDBContext.ToListAsync());
+        }
+
+        // GET: Payment/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payments = await _context.Payments
+                .Include(p => p.Customer)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (payments == null)
+            {
+                return NotFound();
+            }
+
+            return View(payments);
+        }
+
+        // GET: Payment/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "FirstName");
+            return View();
+        }
+
+        // POST: Payment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerId,PmtDate,Amt")] Payments payments)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(payments);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "FirstName", payments.CustomerId);
+            return View(payments);
+        }
+
+        // GET: Payment/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payments = await _context.Payments.FindAsync(id);
+            if (payments == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "FirstName", payments.CustomerId);
+            return View(payments);
+        }
+
+        // POST: Payment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CustomerId,PmtDate,Amt")] Payments payments)
+        {
+            if (id != payments.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(payments);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PaymentsExists(payments.CustomerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "FirstName", payments.CustomerId);
+            return View(payments);
+        }
+
+        // GET: Payment/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payments = await _context.Payments
+                .Include(p => p.Customer)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (payments == null)
+            {
+                return NotFound();
+            }
+
+            return View(payments);
+        }
+
+        // POST: Payment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var payments = await _context.Payments.FindAsync(id);
+            _context.Payments.Remove(payments);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PaymentsExists(int id)
+        {
+            return _context.Payments.Any(e => e.CustomerId == id);
+        }
+    }
+}
